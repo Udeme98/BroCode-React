b4b5888c8b50382b5e8ac8cefa36789ac6347603
Udeme98 <Darealudeme1@gmail.com>
2024-04-12 16:28:37 +0100

    setting the HTML of the app

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,15 +4,30 @@ const TodoList = () => {
   const [tasks, setTasks] = useState([]);
   const [newtask, setNewTask] = useState("");
 
-  const handleInputChange = () => {};
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
   const addTask = () => {};
   const deleteTask = (index) => {};
   const moveTaskUp = (index) => {};
   const moveTaskDown = (index) => {};
 
   return (
-    <div>
-      <h3>Todo List Project</h3>
+    <div className="to-do-list">
+      <h1>Todo List Project</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newtask}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
     </div>
   );
 };
