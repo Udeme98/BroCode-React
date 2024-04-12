4cc97991f12c2d4465bd4efccf66a30b6dfc9254
Udeme98 <Darealudeme1@gmail.com>
2024-04-12 16:19:49 +0100

    set up state and some functions to implement

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newtask, setNewTask] = useState("");
+
+  const handleInputChange = () => {};
+  const addTask = () => {};
+  const deleteTask = (index) => {};
+  const moveTaskUp = (index) => {};
+  const moveTaskDown = (index) => {};
+
   return (
     <div>
       <h3>Todo List Project</h3>
